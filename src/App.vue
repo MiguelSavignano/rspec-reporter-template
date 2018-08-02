0c33a3affb6cddb03aa59ae72750d96b70311923
Miguel Savignano <migue.masx@gmail.com>
2018-08-02 04:16:27 +0200

    colums 1/3

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.section-1 {
+  display: flex;
+}
+.section-1 > * {
+  width: 33%;
 }
 </style>
