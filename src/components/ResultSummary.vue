c5b0b24e9d020af28772f9e575edca9f8870da43
Miguel Savignano <migue.masx@gmail.com>
2018-08-02 12:40:57 +0200

    show result summary

--- src/components/ResultSummary.vue
+++ src/components/ResultSummary.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-    <h1>Navbar</h1>
+  <div :summary="summary" :summary_line="summary_line" >
+    <h1>Result summary</h1>
+    <h3>{{summary_line}}</h3>
+    <br>
+    <ul class="summary-list">
+      <li><b>Duration: </b>{{summary.duration}}</li>
+      <li><b>Example count: </b>{{summary.example_count}}</li>
+      <li><b>Failure count: </b>{{summary.failure_count}}</li>
+      <li><b>Pending count: </b>{{summary.pending_count}}</li>
+      <li><b>Errors outside of examples count: </b>{{summary.errors_outside_of_examples_count}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import data from "../model"
+import Data from "../model"
 
 export default {
   data() {
-    return {}
+    return { summary: Data.summary, summary_line: Data.summary_line }
   }
 }
 </script>
+<style>
+.summary-list {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 24%;
+}
+</style>
