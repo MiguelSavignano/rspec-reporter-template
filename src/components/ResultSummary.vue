85f32c72f691c01a47bc4e6c3a2c5b8ce819635b
Miguel Savignano <migue.masx@gmail.com>
2018-08-03 02:08:36 +0200

    .

--- src/components/ResultSummary.vue
+++ src/components/ResultSummary.vue
@@ -1,16 +1,4 @@
 <template>
-  <!-- <div  class="summary-container"  >
-    <h1>Result summary</h1>
-    <h3>{{summary_line}}</h3>
-    <br>
-    <ul class="summary-list">
-      <li><b>Duration: </b>{{summary.duration}}</li>
-      <li><b>Example count: </b>{{summary.example_count}}</li>
-      <li><b>Failure count: </b>{{summary.failure_count}}</li>
-      <li><b>Pending count: </b>{{summary.pending_count}}</li>
-      <li><b>Errors outside of examples count: </b>{{summary.errors_outside_of_examples_count}}</li>
-    </ul>
-  </div> -->
 <div class="result-summary-container" :summary="summary" :summary_line="summary_line" >
   <div class="card">
   <header class="card-header">
