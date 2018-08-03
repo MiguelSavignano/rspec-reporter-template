58c348dd8579a831555541e301a0c1215d65eafc
Miguel Savignano <migue.masx@gmail.com>
2018-08-03 03:00:48 +0200

    node ejs build html app

--- build-html-app.js
+++ build-html-app.js
@@ -1,17 +1,25 @@
 const ejs = require("ejs")
 var fs = require('fs');
 
-const renderFilePromise = new Promise((resolve, reject) => {
-  ejs.renderFile("index.html.ejs", {name: "Miguel"}, {}, function(err, str){
+const appJsFilePath = "./dist/App.umd.min.js"
+const appCssFilePath = "./dist/App.css"
+const fileTemplatePath = "index.html.ejs"
+const outputFileTemplatePath = "index.html"
+
+const noEscape = (string) => string
+
+new Promise((resolve, reject) => {
+  var APP_JS = fs.readFileSync(appJsFilePath, 'utf8')
+  var APP_CSS = fs.readFileSync(appCssFilePath, 'utf8')
+  ejs.renderFile(fileTemplatePath, {APP_JS, APP_CSS}, {escape: noEscape}, function(err, str){
     if(err) {
       console.error(err)
       reject(err)
     }
-    console.log(str)
-
-    fs.writeFile("index.html", str, function(err) {
+    // console.log(str)
+    fs.writeFile(outputFileTemplatePath, str, function(err) {
       if(err) { return console.error(err) }
-    });
+    })
     resolve(str)
   });
 })
